fa1fa96dd5e39706c30989f1fa03283b45e82739
Matthew Cataldi & Qymana Botts <pair+mjcataldi.qymanab@devbootcamp.com>
2017-07-28 12:30:35 -0500

    Saved local

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -2,3 +2,17 @@ get "/answers" do
 
   erb :"/answers/index"
 end
+
+post "/answers" do
+  @answer = Answer.new(question_id: params[:question_id], body: params[:body], author: current_user)
+
+  if @answer.save
+    if request.xhr?
+      # @answer
+    else
+      redirect "/questions/#{@answer.question.id}"
+    end
+
+  end
+
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,26 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // BUTTON TO SHOW ANSWER FORM
+  $("button.add").on("click", function(){
+    event.preventDefault();
+
+    $(this).nextAll("form.add-form").slideToggle();
+  });
+
+  // ANSWER FORM ON SUBMIT AJAX
+  $("form.add-form").on("submit", function(event){
+    event.preventDefault();
+    var $this = $(this);
+
+    var request = $.ajax({
+      url: $this.attr("action"),
+      type: $this.attr("method"),
+      data: $this.serialize()
+    });
+
+
+  });
+
+
+
 });
