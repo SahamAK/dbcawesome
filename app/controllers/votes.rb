7e6d49b45204c13f6019aca389972008938ec56d
Erik Wotring & Saham Khozestani <pair+ewotring.sahamak@devbootcamp.com>
2017-07-28 15:57:14 -0500

    add upvote to add a +1 in the votable table for the question, add downvote to add a -1 in the votable table for the question in the database

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,4 +1,10 @@
 get "/votes" do
-
   erb :"/votes/index"
+end
+
+post '/questions/:id/vote' do
+  @question = Question.find(params[:id])
+  @question.votes.create(voter: current_user, vote_value: params[:vote_value])
+
+  redirect '/questions'
 end
