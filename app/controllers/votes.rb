c9baedde1cfb7af7e05d8b096ec91ae38938891d
Erik Wotring & Saham Khozestani <pair+ewotring.sahamak@devbootcamp.com>
2017-07-28 16:09:58 -0500

    write the method to show the total number of votes for the questions without ajax

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -5,6 +5,8 @@ end
 post '/questions/:id/vote' do
   @question = Question.find(params[:id])
   @question.votes.create(voter: current_user, vote_value: params[:vote_value])
+  # use the following line to ajax here
+  # p @question.votes.sum(:vote_value)
 
   redirect '/questions'
 end
